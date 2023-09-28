37d26ebc458db8b2cfcd14e27416183ebc00e25d
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-09-28 22:27:43 +0300

    Initial commit

---
+++ lib/fast_json/schema.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "schema/version"
+
+module FastJson
+  module Schema
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/fast_json/schema_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FastJson::Schema do
+  it "has a version number" do
+    expect(FastJson::Schema::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

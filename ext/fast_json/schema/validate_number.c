7eb8e3dcb9b3c52d3c59e35ea727323fefda7872
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-10-11 01:36:34 +0300

    Number `multipeOf` validation

--- ext/fast_json/schema/validate_number.c
+++ ext/fast_json/schema/validate_number.c
@@ -4,4 +4,27 @@
 void validate_number(VALUE schema, CompiledSchema *compiled_schema, VALUE data, Context *context) {
   if(!RB_INTEGER_TYPE_P(data) && !RB_TYPE_P(data, T_FLOAT))
     return yield_error(compiled_schema, data, context, "type_number");
+
+  double data_c;
+
+  if(RB_INTEGER_TYPE_P(data)) {
+    data_c = NUM2DBL(data);
+  } else {
+    data_c = NUM2LONG(data);
+  }
+
+
+  if(RB_INTEGER_TYPE_P(compiled_schema->multipleOf_val)) {
+    long multipleOf = NUM2LONG(compiled_schema->multipleOf_val);
+    double div = data_c / multipleOf;
+
+    if(div - (int)div != 0.0) // fmod() does not work here due to IEEE floating point arithmetics
+      yield_error(compiled_schema, data, context, "multipleOf");
+  } else if(RB_TYPE_P(compiled_schema->multipleOf_val, T_FLOAT)) {
+    double multipleOf = NUM2DBL(compiled_schema->multipleOf_val);
+    double div = data_c / multipleOf;
+
+    if(div - (int)div != 0.0) // fmod() does not work here due to IEEE floating point arithmetics
+      yield_error(compiled_schema, data, context, "multipleOf");
+  }
 }
---
+++ spec/fast_json/validations/number/multiple_of_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe "FastJSON::Schema Number" do
+  describe "multipleOf" do
+    let(:ruby_schema) { { "type" => "number", "multipleOf" => multiple_of } }
+    let(:schema) { FastJSON::Schema.create(ruby_schema) }
+
+    describe "valid?" do
+      using RSpec::Parameterized::TableSyntax
+
+      where(:data, :multiple_of, :valid?) do
+        4.0 | 1   | true
+        4.0 | 0.2 | true
+        4   | 1   | true
+        4   | 0.2 | true
+        4.0 | 3   | false
+        4.0 | 0.3 | false
+        4   | 3   | false
+        4   | 0.3 | false
+      end
+
+      with_them do
+        subject { schema.valid?(data) }
+
+        it { is_expected.to be(valid?) }
+      end
+    end
+  end
+end

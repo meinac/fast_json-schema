5a96e0ee64f7db6a12f5ed92939283bad8a66940
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-10-19 22:14:40 +0300

    Format keywords source and header file

--- ext/fast_json/schema/keywords.c
+++ ext/fast_json/schema/keywords.c
@@ -2,23 +2,56 @@
 
 VALUE root_path_str;
 
-VALUE id_str, ref_str, recursiveAnchor_str, recursiveRef_str;
+VALUE id_str,
+      ref_str,
+      recursiveAnchor_str,
+      recursiveRef_str;
 
-VALUE type_str, const_str, enum_str;
+VALUE type_str,
+      const_str,
+      enum_str;
 
-VALUE if_str, then_str, else_str, allOf_str, anyOf_str, oneOf_str, not_str;
+VALUE if_str,
+      then_str,
+      else_str,
+      allOf_str,
+      anyOf_str,
+      oneOf_str,
+      not_str;
 
 // Integer and Number keywords
-VALUE multipleOf_str, maximum_str, exclusiveMaximum_str, minimum_str, exclusiveMinimum_str;
+VALUE multipleOf_str,
+      maximum_str,
+      exclusiveMaximum_str,
+      minimum_str,
+      exclusiveMinimum_str;
 
 // String keywords
-VALUE maxLength_str, minLength_str, pattern_str, format_str;
+VALUE maxLength_str,
+      minLength_str,
+      pattern_str,
+      format_str;
 
 // Array keywords
-VALUE items_str, additionalItems_str, unevaluatedItems_str, contains_str, maxItems_str, minItems_str, uniqueItems_str, maxContains_str, minContains_str;
+VALUE items_str,
+      additionalItems_str,
+      unevaluatedItems_str,
+      contains_str,
+      maxItems_str,
+      minItems_str,
+      uniqueItems_str,
+      maxContains_str,
+      minContains_str;
 
 // Object keywords
-VALUE properties_str, patternProperties_str, additionalProperties_str, unevaluatedProperties_str, maxProperties_str, minProperties_str, required_str, dependentRequired_str;
+VALUE properties_str,
+      patternProperties_str,
+      additionalProperties_str,
+      unevaluatedProperties_str,
+      maxProperties_str,
+      minProperties_str,
+      required_str,
+      dependentRequired_str;
 
 void Init_keywords() {
   root_path_str = rb_str_new_literal("#");
--- ext/fast_json/schema/keywords.h
+++ ext/fast_json/schema/keywords.h
@@ -2,18 +2,51 @@
 
 extern VALUE root_path_str;
 
-extern VALUE id_str, ref_str, recursiveAnchor_str, recursiveRef_str;
+extern VALUE id_str,
+             ref_str,
+             recursiveAnchor_str,
+             recursiveRef_str;
 
-extern VALUE type_str, const_str, enum_str;
+extern VALUE type_str,
+             const_str,
+             enum_str;
 
-extern VALUE if_str, then_str, else_str, allOf_str, anyOf_str, oneOf_str, not_str;
+extern VALUE if_str,
+             then_str,
+             else_str,
+             allOf_str,
+             anyOf_str,
+             oneOf_str,
+             not_str;
 
-extern VALUE multipleOf_str, maximum_str, exclusiveMaximum_str, minimum_str, exclusiveMinimum_str;
+extern VALUE multipleOf_str,
+             maximum_str,
+             exclusiveMaximum_str,
+             minimum_str,
+             exclusiveMinimum_str;
 
-extern VALUE maxLength_str, minLength_str, pattern_str, format_str;
+extern VALUE maxLength_str,
+             minLength_str,
+             pattern_str,
+             format_str;
 
-extern VALUE items_str, additionalItems_str, unevaluatedItems_str, contains_str, maxItems_str, minItems_str, uniqueItems_str, maxContains_str, minContains_str;
+extern VALUE items_str,
+             additionalItems_str,
+             unevaluatedItems_str,
+             contains_str,
+             maxItems_str,
+             minItems_str,
+             uniqueItems_str,
+             maxContains_str,
+             minContains_str;
 
-extern VALUE properties_str, patternProperties_str, additionalProperties_str, unevaluatedProperties_str, maxProperties_str, minProperties_str, required_str, dependentRequired_str;
+extern VALUE properties_str,
+             patternProperties_str,
+             additionalProperties_str,
+             unevaluatedProperties_str,
+             maxProperties_str,
+             minProperties_str,
+             required_str,
+             dependentRequired_str;
 
 void Init_keywords();

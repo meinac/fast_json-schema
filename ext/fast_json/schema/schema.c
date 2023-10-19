410798d23f2324c0be39f8760726fc8ecc4f78f3
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-10-19 21:22:23 +0300

    Remove the hack of casting VALUE to pointer for Context

--- ext/fast_json/schema/schema.c
+++ ext/fast_json/schema/schema.c
@@ -3,39 +3,24 @@
 #include "compiled_schema.h"
 #include "types/context.h"
 #include "error.h"
-#include "value_pointer_caster.h"
 
-VALUE rb_validate_with_schema(VALUE self, VALUE compiled_schema_obj, VALUE data, VALUE context_pointer) {
+VALUE rb_validate_with_schema(VALUE self, VALUE compiled_schema_obj, VALUE data) {
   if (!rb_block_given_p()) {
-    VALUE args[3] = { compiled_schema_obj, data, context_pointer};
+    VALUE args[2] = { compiled_schema_obj, data };
 
-    RETURN_SIZED_ENUMERATOR(self, 3, args, 0);
+    RETURN_SIZED_ENUMERATOR(self, 2, args, 0);
   }
 
   CompiledSchema *compiled_schema;
   GetCompiledSchema(compiled_schema_obj, compiled_schema);
 
   Context context_s;
-  Context *context;
 
-  /*
-  * TODO:
-  * I am highly concerned about the security of this hack as users can provide
-  * integer values to this method which can point to a random place in memory.
-  * Maybe I should return back to using Ruby typed Context struct.
-  * I will sleep on this idea.
-  */
-  if(NIL_P(context_pointer)) {
-    context_s.path[0] = root_path_str;
-    context_s.depth = 0;
-    context_s.jump = false;
+  context_s.path[0] = root_path_str;
+  context_s.depth = 0;
+  context_s.jump = false;
 
-    context = &context_s;
-  } else {
-    context = NUM2PTR(context_pointer);
-  }
-
-  compiled_schema->validation_function(self, compiled_schema, data, context);
+  compiled_schema->validation_function(self, compiled_schema, data, &context_s);
 
   return Qnil;
 }
@@ -43,7 +28,7 @@ VALUE rb_validate_with_schema(VALUE self, VALUE compiled_schema_obj, VALUE data,
 VALUE rb_validate(VALUE self, VALUE data) {
   VALUE compiled_schema_obj = rb_ivar_get(self, rb_intern("compiled_schema"));
 
-  return rb_funcall(self, rb_intern("validate_with_schema"), 3, compiled_schema_obj, data, Qnil);
+  return rb_funcall(self, rb_intern("validate_with_schema"), 2, compiled_schema_obj, data);
 }
 
 VALUE rb_compile(VALUE self) {
@@ -64,5 +49,5 @@ void Init_schema() {
 
   rb_define_method(schema_class, "compile", rb_compile, 0);
   rb_define_method(schema_class, "validate", rb_validate, 1);
-  rb_define_private_method(schema_class, "validate_with_schema", rb_validate_with_schema, 3);
+  rb_define_private_method(schema_class, "validate_with_schema", rb_validate_with_schema, 2);
 }

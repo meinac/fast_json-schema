739443c6f89569619ed920dc1e8ca400126b7d85
Mehmet Emin INAC <mehmetemininac@gmail.com>
2026-05-19 05:00:01 +0300

    Fix string length validations

--- ext/fast_json/schema/validate_string.c
+++ ext/fast_json/schema/validate_string.c
@@ -7,12 +7,16 @@ void validate_string(VALUE schema, CompiledSchema *compiled_schema, VALUE data, 
     return yield_error(compiled_schema, data, context, "type_string");
 
   if(compiled_schema->maxLength_val != Qundef) {
-    if(RSTRING_LEN(data) > NUM2LONG(compiled_schema->maxLength_val))
+    VALUE string_length = rb_str_length(data);
+
+    if(NUM2LONG(string_length) > NUM2LONG(compiled_schema->maxLength_val))
       yield_error(compiled_schema, data, context, "maxLength");
   }
 
   if(compiled_schema->minLength_val != Qundef) {
-    if(RSTRING_LEN(data) < NUM2LONG(compiled_schema->minLength_val))
+    VALUE string_length = rb_str_length(data);
+
+    if(NUM2LONG(string_length) < NUM2LONG(compiled_schema->minLength_val))
       yield_error(compiled_schema, data, context, "minLength");
   }
 
